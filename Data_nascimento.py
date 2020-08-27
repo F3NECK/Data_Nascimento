060e4e8775bea8e4e8a875d0ed3bb06b0df7275e
Felipe Magno <felipehmagno@gmail.com>
2020-08-27 09:26:12 -0300

    print("alteração")

--- Data_nascimento.py
+++ Data_nascimento.py
@@ -1,8 +1,8 @@
 "calculo da data de nascimento do individuo"
 
 "entrada"
-print("bem vindo a falsa inteligencia artificial")
-nome = input("para continuarmos ensira seu nome: ")
+print("Bem vindo a falsa inteligencia artificial")
+nome = input("Para continuarmos ensira seu nome: ")
 datanasc = int(input("Qual seu ano de Nascimento? "))
 idade = 2020 - datanasc
 dias = idade * 365
